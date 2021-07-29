fe4a59f0e3e73fa6a2655cce3ee5fe894e621682
Janderson Cassan <jande.max@gmail.com>
2021-07-28 21:09:33 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/mscloud/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscloud.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
