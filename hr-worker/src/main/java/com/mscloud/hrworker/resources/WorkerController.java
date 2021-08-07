0e4fc77916db7e316bdcb5b81dccc6f425b9c9f3
Janderson Cassan <jande.max@gmail.com>
2021-08-06 22:19:04 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/mscloud/hrworker/resources/WorkerController.java
+++ hr-worker/src/main/java/com/mscloud/hrworker/resources/WorkerController.java
@@ -2,6 +2,7 @@ package com.mscloud.hrworker.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,16 @@ public class WorkerController {
 	
 	private final Environment env;	
 	private final WorkerService service;
+
+	@Value("${test.config}")
+	private String textConfig;
 	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> testConfigs(){
+		log.info("CONFIG: {}", textConfig);
+		return ResponseEntity.noContent().build();
+	}
+
 	@GetMapping
 	public ResponseEntity<List<WorkerDTO>> findAll(){
 		List<WorkerDTO> list = service.findAll();
