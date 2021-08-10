b771252791ce2b156a72f09234c739ceb31a05d3
Janderson Cassan <jande.max@gmail.com>
2021-08-10 19:55:59 -0300

    Database seeding

--- hr-user/src/main/java/com/mscloud/hruser/entities/User.java
+++ hr-user/src/main/java/com/mscloud/hruser/entities/User.java
@@ -36,7 +36,10 @@ public class User implements Serializable{
 	private String password;
 	
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name="tb_user_role", 
+				joinColumns = @JoinColumn(name = "user_id"),
+				inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
 	private Set<Role> roles = new HashSet<>();
 	
 	@Override
